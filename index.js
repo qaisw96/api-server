1b23dac6c0957bfc27a31da3ae396fb1ff06e405
qaisw96 <qaisw14@gmail.com>
2021-05-15 22:52:59 +0300

    DB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // require dotenv package to import variables from .env file 
 require('dotenv').config();
 const server = require('./src/server');
+// import variables from .env
 const {PORT, MONGODB_URI} = process.env;
 
 
